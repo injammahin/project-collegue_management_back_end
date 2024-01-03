b6653ab975697e35a6035403375b772754d5d587
injammahin <injammahin5507@gmail.com>
2024-01-03 15:53:30 +0600

    up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(6000);
+
+  // Define your CORS options
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', // or an array of allowed origins ['http://localhost:3000']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable credentials (cookies, HTTP authentication) in the CORS request
+  };
+
+  // Enable CORS using the CorsMiddleware
+  app.enableCors(corsOptions);
+
+  await app.listen(3001);
 }
 bootstrap();
